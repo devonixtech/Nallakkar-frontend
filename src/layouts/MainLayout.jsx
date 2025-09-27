cfabdf12fc6f065de39c80f4757a335b0e272c0b
sanjana patel <sanjanapatel438@gmail.com>
2025-09-27 12:47:15 +0530

    'footerchannge''

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,25 +1,61 @@
 // import { Outlet } from "react-router-dom";
 // import CategoryNavbar from "../components/layout/CategoryNavbar";
 // import Footer from "../components/layout/Footer";
+// import MobileBottomNav from "../components/layout/MobileBottomNav"; // 👈 import here
 
 // export default function MainLayout() {
 //   return (
 //     <>
-//       <CategoryNavbar />
-//       <main className="min-h-[calc(100vh-200px)]">
+//       {/* Desktop Navbar */}
+//       <div className="hidden md:block">
+//         <CategoryNavbar />
+//       </div>
+
+//       {/* Page Content */}
+//       <main className="min-h-[calc(100vh-200px)] pt-0 lg:pt-20">
 //         <Outlet />
 //       </main>
-//       <Footer />
+
+//       {/* Desktop Footer */}
+//       <div className="hidden md:block">
+//         <Footer />
+//       </div>
+
+//       {/* Mobile Bottom Navigation */}
+//       <div className="md:hidden fixed bottom-0 left-0 w-full border-t bg-white z-50">
+//         <MobileBottomNav />
+//       </div>
 //     </>
 //   );
 // }
 
-import { Outlet } from "react-router-dom";
+
+import { Outlet, useLocation } from "react-router-dom";
 import CategoryNavbar from "../components/layout/CategoryNavbar";
 import Footer from "../components/layout/Footer";
-import MobileBottomNav from "../components/layout/MobileBottomNav"; // 👈 import here
+import MobileBottomNav from "../components/layout/MobileBottomNav";
 
 export default function MainLayout() {
+  const location = useLocation();
+
+  // jahan footer hide karna hai
+  const hideFooterRoutes = [
+    "/wishlist",
+    "/cart",
+    "/buyNow",
+    "/SelectAddress",
+    "/AddDeliveryAddress",
+    "/ProductOverview",
+    "/PaymentPage",
+    "/PaymentSuccess",
+    "/OrderConfirmation",
+    "/WriteReview",
+  ];
+
+  const shouldHideFooter = hideFooterRoutes.some((route) =>
+    location.pathname.startsWith(route)
+  );
+
   return (
     <>
       {/* Desktop Navbar */}
@@ -32,10 +68,12 @@ export default function MainLayout() {
         <Outlet />
       </main>
 
-      {/* Desktop Footer */}
-      <div className="hidden md:block">
-        <Footer />
-      </div>
+      {/* Desktop Footer (conditionally render) */}
+      {!shouldHideFooter && (
+        <div className="hidden md:block">
+          <Footer />
+        </div>
+      )}
 
       {/* Mobile Bottom Navigation */}
       <div className="md:hidden fixed bottom-0 left-0 w-full border-t bg-white z-50">
